b17bd171f719c618828084ce1adf3f88c266d44a
Harshit Gupta <26harshitgupta99@gmail.com>
2020-04-23 16:47:29 +0530

    Updates

---
+++ precache-manifest.121ba262d20c2dd764f226bfae3c2b8a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4924c181fa9d5c550838637a99d557d4",
+    "url": "/Contact-Manager/index.html"
+  },
+  {
+    "revision": "944bc5422e2e018b03e0",
+    "url": "/Contact-Manager/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "5c39d24191572324b251",
+    "url": "/Contact-Manager/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "944bc5422e2e018b03e0",
+    "url": "/Contact-Manager/static/js/2.5be3f437.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Contact-Manager/static/js/2.5be3f437.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c39d24191572324b251",
+    "url": "/Contact-Manager/static/js/main.d0084076.chunk.js"
+  },
+  {
+    "revision": "fe23eaddc965d28be32e",
+    "url": "/Contact-Manager/static/js/runtime-main.88a330d6.js"
+  }
+]);
